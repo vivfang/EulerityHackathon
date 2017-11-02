ffc13ff4000d7c535043eae5d9b384658e3f985c
Joe Ciaramitaro <joe@eulerity.com>
2017-11-02 12:29:20 -0400

    updates

--- src/main/java/com/eulerity/hackathon/App.java
+++ src/main/java/com/eulerity/hackathon/App.java
@@ -1,4 +1,4 @@
-package com.eulerity.hackathon2;
+package com.eulerity.hackathon;
 
 /**
  * Hello world!
--- src/test/java/com/eulerity/hackathon/AppTest.java
+++ src/test/java/com/eulerity/hackathon/AppTest.java
@@ -1,4 +1,4 @@
-package com.eulerity.hackathon2;
+package com.eulerity.hackathon;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
